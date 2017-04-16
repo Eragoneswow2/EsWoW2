f14e2ad3df7293aeccb35262160831f3a69083ff
Eragoneswow2 <david_7_8_7@hotmail.com>
2017-04-16 12:02:40 +0200

    Añadida Guarida de Alanegra.

---
+++ 3.3.5 - TDB/Bandas CLASSIC/[EsWoW2] Guarida de Alanegra.sql
@@ -0,0 +1,69 @@
+﻿
+-- GUARIDA DE ALANEGRA --
+
+-- Jefe de Guerra Rend Puño Negro --
+UPDATE `locales_creature_text` SET `entry`='10429', `groupid`='0', `id`='0', `text_loc6`='¡Imposible!' WHERE (`entry`='10429') AND (`groupid`='0') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='10429', `groupid`='1', `id`='0', `text_loc6`='Con mucho gusto...' WHERE (`entry`='10429') AND (`groupid`='1') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='10429', `groupid`='2', `id`='0', `text_loc6`='%s es derribado del draco.' WHERE (`entry`='10429') AND (`groupid`='2') AND (`id`='0');
+
+-- Lord Victor Nefarius --
+UPDATE `locales_creature_text` SET `entry`='10162', `groupid`='0', `id`='0', `text_loc6`='Excelente... es como si esos entrometidos insectos vinieran justo a tiempo para servir de comida a mi legión. ¡Bienvenidos, mortales!' WHERE (`entry`='10162') AND (`groupid`='0') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='10162', `groupid`='1', `id`='0', `text_loc6`='No dejéis ni una sola gota de su sangre en el suelo, hijos míos. ¡Devorad sus almas!' WHERE (`entry`='10162') AND (`groupid`='1') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='10162', `groupid`='2', `id`='0', `text_loc6`='Idiotassss... ¡Matad al del vestido!' WHERE (`entry`='10162') AND (`groupid`='2') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='10162', `groupid`='3', `id`='0', `text_loc6`='¡Inconcebible!' WHERE (`entry`='10162') AND (`groupid`='3') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='10162', `groupid`='4', `id`='0', `text_loc6`='Vuestros esfuerzos resultarán inútiles ¡Nada se interpondrá en nuestro camino!' WHERE (`entry`='10162') AND (`groupid`='4') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='10162', `groupid`='5', `id`='0', `text_loc6`='¡No me obligues, hijo! O usaré tus pieles para forrar mis botas.' WHERE (`entry`='10162') AND (`groupid`='5') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='10162', `groupid`='6', `id`='0', `text_loc6`='¡Usa el aliento helado, imbécil!' WHERE (`entry`='10162') AND (`groupid`='6') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='10162', `groupid`='7', `id`='0', `text_loc6`='¡¡NO PUEDE SER!! Rend, encárgate de esos insectos.' WHERE (`entry`='10162') AND (`groupid`='7') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='10162', `groupid`='8', `id`='0', `text_loc6`='El Jefe de Guerra será rápido con vosotros, mortales. ¡Preparaos!' WHERE (`entry`='10162') AND (`groupid`='8') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='10162', `groupid`='9', `id`='0', `text_loc6`='¡Prueba mi poder!' WHERE (`entry`='10162') AND (`groupid`='9') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='10162', `groupid`='10', `id`='0', `text_loc6`='Esta victoria durará poco. ¡Los días divididos entre la Horda y la Alianza están llegando a su fin! La próxima vez que nos encontremos será la última.' WHERE (`entry`='10162') AND (`groupid`='10') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='10162', `groupid`='11', `id`='0', `text_loc6`='¡Los mortales han caído! ¡La nueva era de la legión de Roca Negra está cerca!' WHERE (`entry`='10162') AND (`groupid`='11') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='10162', `groupid`='12', `id`='0', `text_loc6`='En este mundo dónde el tiempo es tu enemigo, es mi mayor aliado. Este gran juego de la vida que crees jugar, en realidad juega contigo. Y yo digo...' WHERE (`entry`='10162') AND (`groupid`='12') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='10162', `groupid`='13', `id`='0', `text_loc6`='¡Que comiencen los juegos!' WHERE (`entry`='10162') AND (`groupid`='13') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='10162', `groupid`='14', `id`='0', `text_loc6`='Ah... Los héroes. Sois obstinados, ¿verdad? Vuestro aliado ha intentado medir nuestros poderes y ha pagado el precio por ello. Ahora él me servirá... y os descuartizará. ¡Levántate dragoncito rojo... y destrúyelos!' WHERE (`entry`='10162') AND (`groupid`='14') AND (`id`='0');
+
+-- Grethok el Controlador --
+UPDATE `locales_creature_text` SET `entry`='12557', `groupid`='0', `id`='0', `text_loc6`='¡Los invasores han penetrado en El Criadero! ¡Activad la alarma! ¡Hay que proteger los huevos a toda costa!' WHERE (`entry`='12557') AND (`groupid`='0') AND (`id`='0');
+
+-- Sangrevaja el Indomable --
+UPDATE `locales_creature_text` SET `entry`='12435', `groupid`='0', `id`='0', `text_loc6`='¡Pagaréis por obligarme a hacer esto!' WHERE (`entry`='12435') AND (`groupid`='0') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='12435', `groupid`='1', `id`='0', `text_loc6`='¡Locos! ¡Esos huevos son más valiosos de lo que creéis!' WHERE (`entry`='12435') AND (`groupid`='1') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='12435', `groupid`='2', `id`='0', `text_loc6`='¡No, otro no! ¡Rodarán vuestras cabezas por esta infamia!' WHERE (`entry`='12435') AND (`groupid`='2') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='12435', `groupid`='3', `id`='0', `text_loc6`='¡Si yo caigo en el abismo, vosotros mortales vendréis todos conmigo!' WHERE (`entry`='12435') AND (`groupid`='3') AND (`id`='0');
+
+-- Vaelastrasz el Corrupto --
+UPDATE `locales_creature_text` SET `entry`='13020', `groupid`='0', `id`='0', `text_loc6`='¡Demasiado tarde, amigos! Ahora estoy poseído por la corrupción de Nefarius... No puedo... controlarme.' WHERE (`entry`='13020') AND (`groupid`='0') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='13020', `groupid`='1', `id`='0', `text_loc6`='Os lo ruego, mortales. ¡ESCAPAD! ¡Escapad antes de que pierda todo el control! ¡Las llamaradas negras se extienden en mi corazón! ¡DEBO liberarlas!' WHERE (`entry`='13020') AND (`groupid`='1') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='13020', `groupid`='2', `id`='0', `text_loc6`='¡FUEGO! ¡MUERTE! ¡DESTRUCCIÓN! Postraos, mortales, ante la ira del Señor... ¡NO, DEBO luchar contra esto! ¡Alexstrasza ayúdame, tengo que combatirlo!' WHERE (`entry`='13020') AND (`groupid`='2') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='13020', `groupid`='3', `id`='0', `text_loc6`='¡El odio de Nefarius me ha hecho más fuerte que nunca! ¡Deberíais haber escapado mientras podíais, mortales! ¡La furia de Roca Negra corre por mis venas!' WHERE (`entry`='13020') AND (`groupid`='3') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='13020', `groupid`='4', `id`='0', `text_loc6`='¡Perdóname, $N! ¡Tu muerte sólo aumenta mi fracaso!' WHERE (`entry`='13020') AND (`groupid`='4') AND (`id`='0');
+
+-- Señor de linaje Capazote --
+UPDATE `locales_creature_text` SET `entry`='12017', `groupid`='0', `id`='0', `text_loc6`='¡Nadie de vuestra raza debería estar aquí! ¡Os aniquilaré!' WHERE (`entry`='12017') AND (`groupid`='0') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='12017', `groupid`='1', `id`='0', `text_loc6`='¡Inteligente, mortales! ¡Pero no me atraeréis tan lejos de mi santuario tan fácilmente!' WHERE (`entry`='12017') AND (`groupid`='1') AND (`id`='0');
+
+-- Flamagor --
+UPDATE `locales_creature_text` SET `entry`='11981', `groupid`='0', `id`='0', `text_loc6`='¡%s se enfurece!' WHERE (`entry`='11981') AND (`groupid`='0') AND (`id`='0');
+
+-- Chromaggus --
+UPDATE `locales_creature_text` SET `entry`='14020', `groupid`='0', `id`='0', `text_loc6`='%s entra en un frenesí de muerte.' WHERE (`entry`='14020') AND (`groupid`='0') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='14020', `groupid`='1', `id`='0', `text_loc6`='%s se estremece mientras su piel empieza a brillar.' WHERE (`entry`='14020') AND (`groupid`='1') AND (`id`='0');
+
+-- Nefarian --
+UPDATE `locales_creature_text` SET `entry`='11583', `groupid`='0', `id`='0', `text_loc6`='Bien hecho, mis esbirros. El coraje de los mortales empieza a mermar. ¡Veamos ahora cómo se enfrentan al verdadero Señor de la Cumbre de Roca Negra!' WHERE (`entry`='11583') AND (`groupid`='0') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='11583', `groupid`='0', `id`='1', `text_loc6`='¡Basta! Ahora, vermin, sentirás la fuerza de mi legado, la mismísima furia de la tierra.' WHERE (`entry`='11583') AND (`groupid`='0') AND (`id`='1');
+UPDATE `locales_creature_text` SET `entry`='11583', `groupid`='0', `id`='2', `text_loc6`='¡ARDED! ¡Desdichados! ¡ARDED!' WHERE (`entry`='11583') AND (`groupid`='0') AND (`id`='2');
+UPDATE `locales_creature_text` SET `entry`='11583', `groupid`='1', `id`='0', `text_loc6`='¡Imposible! ¡Erguíos, mis esbirros! ¡Servid a vuestro maestro una vez más!' WHERE (`entry`='11583') AND (`groupid`='1') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='11583', `groupid`='2', `id`='0', `text_loc6`='¡Eres un inútil, $N! ¡Tus amigos pronto se reunirán contigo!' WHERE (`entry`='11583') AND (`groupid`='2') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='11583', `groupid`='3', `id`='0', `text_loc6`='¡¡No puede ser cierto!! ¡Yo soy aquí el maestro! ¡Vosotros, mortales, no sois nada para mi raza! ¿Habéis entendido? ¡Nada!' WHERE (`entry`='11583') AND (`groupid`='3') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='11583', `groupid`='4', `id`='0', `text_loc6`='¿Magos también? Deberíais tener más cuidado cuando jugáis con la magia...' WHERE (`entry`='11583') AND (`groupid`='4') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='11583', `groupid`='5', `id`='0', `text_loc6`='¡Guerreros, sé que podéis golpear más fuerte que eso! ¡Venga!' WHERE (`entry`='11583') AND (`groupid`='5') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='11583', `groupid`='6', `id`='0', `text_loc6`='Los druidas y sus cambios de forma. ¡Permitidme verlo en acción!' WHERE (`entry`='11583') AND (`groupid`='6') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='11583', `groupid`='7', `id`='0', `text_loc6`='¡Sacerdotes! ¡Si sois capaces de mantener la curación a este ritmo, puede que podamos hacer de esto algo más interesante!' WHERE (`entry`='11583') AND (`groupid`='7') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='11583', `groupid`='8', `id`='0', `text_loc6`='Paladines... He oído que tenéis mucha vida. Demuéstramelo.' WHERE (`entry`='11583') AND (`groupid`='8') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='11583', `groupid`='9', `id`='0', `text_loc6`='¡Chamanes! ¡Mostradme los tótems que podéis invocar!' WHERE (`entry`='11583') AND (`groupid`='9') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='11583', `groupid`='10', `id`='0', `text_loc6`='No deberíais estar jugando con magia que no entendéis, brujos. ¿Véis lo que pasa?' WHERE (`entry`='11583') AND (`groupid`='10') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='11583', `groupid`='11', `id`='0', `text_loc6`='¡Los cazadores y sus molestas cerbatanas!' WHERE (`entry`='11583') AND (`groupid`='11') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='11583', `groupid`='12', `id`='0', `text_loc6`='¿Pícaros? ¡Dejad de esconderos y enfrentaos a mi!' WHERE (`entry`='11583') AND (`groupid`='12') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='11583', `groupid`='13', `id`='0', `text_loc6`='¡Caballeros de la Muerte... venid aquí!' WHERE (`entry`='11583') AND (`groupid`='13') AND (`id`='0');
