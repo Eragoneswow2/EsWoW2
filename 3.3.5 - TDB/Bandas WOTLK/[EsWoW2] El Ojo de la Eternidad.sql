d170c4d60b87ba4d984b0fd25888e175c1e7f643
Eragoneswow2 <david_7_8_7@hotmail.com>
2017-04-22 15:23:17 +0200

    Añadida El Ojo de la Eternidad.

---
+++ 3.3.5 - TDB/Bandas WOTLK/[EsWoW2] El Ojo de la Eternidad.sql
@@ -0,0 +1,45 @@
+﻿
+-- EL OJO DE LA ETERNIDAD --
+
+-- Malygos --
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='0', `id`='0', `text_loc6`='¡Seres inferiores, inmiscuyéndose aquí! ¡Es una lástima que vuestro exceso de valor no compense vuestra estupidez!' WHERE (`entry`='28859') AND (`groupid`='0') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='0', `id`='1', `text_loc6`='Nadie excepto el Vuelo Azul es bienvenido aquí. ¿Tal vez vengáis de parte de Alexstrasza? ¡Bueno, entonces, os ha enviado a vuestra muerte!' WHERE (`entry`='28859') AND (`groupid`='0') AND (`id`='1');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='0', `id`='2', `text_loc6`='¿Qué esperábais lograr? Apareciendo descaradamente en mis dominios... y empleando magia... ¿contra MI?' WHERE (`entry`='28859') AND (`groupid`='0') AND (`id`='2');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='0', `id`='3', `text_loc6`='Aquí dentro no tengo límites. Las reglas de vuestra realidad no se aplican aquí. En este reino, ¡yo tengo el control!' WHERE (`entry`='28859') AND (`groupid`='0') AND (`id`='3');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='0', `id`='4', `text_loc6`='Os doy una oportunidad. Prométeme fidelidad, y tal vez no os mate por vuestra insolencia.' WHERE (`entry`='28859') AND (`groupid`='0') AND (`id`='4');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='1', `id`='0', `text_loc6`='Habéis agotado mi paciencia. ¡Me desharé de vosotros!' WHERE (`entry`='28859') AND (`groupid`='1') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='2', `id`='0', `text_loc6`='¡No lo conseguiréis mientras me quede aliento!' WHERE (`entry`='28859') AND (`groupid`='2') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='3', `id`='0', `text_loc6`='¡Al fin te ha alcanzado tu estupidez!' WHERE (`entry`='28859') AND (`groupid`='3') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='3', `id`='1', `text_loc6`='Más artefactos que confiscar.' WHERE (`entry`='28859') AND (`groupid`='3') AND (`id`='1');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='3', `id`='2', `text_loc6`='¡Qué ingenuo!' WHERE (`entry`='28859') AND (`groupid`='3') AND (`id`='2');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='4', `id`='0', `text_loc6`='Esperaba acabar con vuestras vidas rápidamente, pero habéis resultado más... resistentes de lo que pensaba. Aun así, vuestros esfuerzos son en vano. Esta guerra es culpa vuestra, imprudentes mortales descuidados. Yo hago lo que debo... y si eso implica vuestra extinción... ¡QUE ASÍ SEA!' WHERE (`entry`='28859') AND (`groupid`='4') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='5', `id`='0', `text_loc6`='¡Pocos han experimentado el dolor que voy a inflingiros ahora!' WHERE (`entry`='28859') AND (`groupid`='5') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='6', `id`='0', `text_loc6`='¡Os enseñaré lo poco que sabéis de la magia, niñatos ignorantes!' WHERE (`entry`='28859') AND (`groupid`='6') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='7', `id`='0', `text_loc6`='¡Mirad indefensos cómo quiebro vuestras esperanzas!' WHERE (`entry`='28859') AND (`groupid`='7') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='8', `id`='0', `text_loc6`='¡Daré buen uso a tu energía!' WHERE (`entry`='28859') AND (`groupid`='8') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='8', `id`='1', `text_loc6`='¡Soy el Tejehechizos! ¡Mi poder es infinito!' WHERE (`entry`='28859') AND (`groupid`='8') AND (`id`='1');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='8', `id`='2', `text_loc6`='¡Tu espíritu permanecerá aquí para siempre!' WHERE (`entry`='28859') AND (`groupid`='8') AND (`id`='2');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='9', `id`='0', `text_loc6`='¡BASTA! ¡Si queréis la magia de Azeroth, la tendréis!' WHERE (`entry`='28859') AND (`groupid`='9') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='10', `id`='0', `text_loc6`='Ahora aparecen vuestros benefactores, ¡pero llegan demasiado tarde! Los poderes aquí contenidos son suficientes para destruir el mundo diez veces. ¿Qué creéis que os harán a vosotros?' WHERE (`entry`='28859') AND (`groupid`='10') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='11', `id`='0', `text_loc6`='¡SOMETEOS!' WHERE (`entry`='28859') AND (`groupid`='11') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='12', `id`='0', `text_loc6`='%s respira hondo.' WHERE (`entry`='28859') AND (`groupid`='12') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='13', `id`='0', `text_loc6`='¡Los poderes que actuan aquí superan cualquier cosa que podáis imaginar!' WHERE (`entry`='28859') AND (`groupid`='13') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='14', `id`='0', `text_loc6`='¡SOY IMPARABLE!' WHERE (`entry`='28859') AND (`groupid`='14') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='15', `id`='0', `text_loc6`='¡Alexstrasza, otra de tus crías cae!' WHERE (`entry`='28859') AND (`groupid`='15') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='15', `id`='1', `text_loc6`='¡Poco más que mosquitos!' WHERE (`entry`='28859') AND (`groupid`='15') AND (`id`='1');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='15', `id`='2', `text_loc6`='¡Vuestros aliados rojos compartirán vuestro mismo destino!' WHERE (`entry`='28859') AND (`groupid`='15') AND (`id`='2');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='16', `id`='0', `text_loc6`='¿Aún sigues en pie? No por mucho tiempo.' WHERE (`entry`='28859') AND (`groupid`='16') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='16', `id`='1', `text_loc6`='¡Tu causa está perdida!' WHERE (`entry`='28859') AND (`groupid`='16') AND (`id`='1');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='16', `id`='2', `text_loc6`='¡Vuestras frágiles mentes serán destrozadas!' WHERE (`entry`='28859') AND (`groupid`='16') AND (`id`='2');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='17', `id`='0', `text_loc6`='¡Impensable! ¡Los mortales lo destruirán... todo! ¿Hermana, qué has...?' WHERE (`entry`='28859') AND (`groupid`='17') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='18', `id`='0', `text_loc6`='¡%s fija su mirada en ti!' WHERE (`entry`='28859') AND (`groupid`='18') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='28859', `groupid`='19', `id`='0', `text_loc6`='¡%s monta en cólera!' WHERE (`entry`='28859') AND (`groupid`='19') AND (`id`='0');
+
+-- Alexstrasza la Protectora  --
+UPDATE `locales_creature_text` SET `entry`='32295', `groupid`='0', `id`='0', `text_loc6`='Hice lo que tenía que hacer, hermano. No me dejaste otra alternativa.' WHERE (`entry`='32295') AND (`groupid`='0') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='32295', `groupid`='1', `id`='0', `text_loc6`='Y así acaba la guerra de El Nexo.' WHERE (`entry`='32295') AND (`groupid`='1') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='32295', `groupid`='2', `id`='0', `text_loc6`='Esta resolución me apena enormemente, pero la destrucción, la monumental pérdida de vida... tenían que acabar. A pesar de las recientes transgresiones de Malygos, lloraré su pérdida. Hubo un tiempo en que fue un guardián, un protector. Hoy ha caído uno de los seres más poderosos del mundo.' WHERE (`entry`='32295') AND (`groupid`='2') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='32295', `groupid`='3', `id`='0', `text_loc6`='El Vuelo Rojo se encargará de arreglar la devastación infligida sobre Azeroth. Regresad a casa con vuestra gente y descansad. El mañana os traerá nuevos retos y tenéis que estar preparados para enfrentaros a ellos. La vida... continúa.' WHERE (`entry`='32295') AND (`groupid`='3') AND (`id`='0');
+
+-- Chispazo --
+UPDATE `locales_creature_text` SET `entry`='30084', `groupid`='0', `id`='0', `text_loc6`='¡Un %s toma forma a partir de una falla cercana!' WHERE (`entry`='30084') AND (`groupid`='0') AND (`id`='0');
