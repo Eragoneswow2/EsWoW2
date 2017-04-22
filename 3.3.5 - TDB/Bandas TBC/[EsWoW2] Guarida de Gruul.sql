4ecd5953908081cf893bd783c8451449f54d240b
Eragoneswow2 <david_7_8_7@hotmail.com>
2017-04-22 10:16:59 +0200

    Añadida Guarida de Gruul.

---
+++ 3.3.5 - TDB/Bandas TBC/[EsWoW2] Guarida de Gruul.sql
@@ -0,0 +1,26 @@
+﻿
+-- GUARIDA DE GRUUL --
+
+-- Su majestad Maulgar --
+UPDATE `locales_creature_text` SET `entry`='18831', `groupid`='0', `id`='0', `text_loc6`='¡Los Gronn son el auténtico poder de Terrallende!' WHERE (`entry`='18831') AND (`groupid`='0') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='18831', `groupid`='1', `id`='0', `text_loc6`='¡No derrotaréis a la mano de Gruul!' WHERE (`entry`='18831') AND (`groupid`='1') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='18831', `groupid`='2', `id`='0', `text_loc6`='No mataréis al próximo tan fácilmente.' WHERE (`entry`='18831') AND (`groupid`='2') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='18831', `groupid`='2', `id`='1', `text_loc6`='¡Bah! ¡Esto no prueba nada!' WHERE (`entry`='18831') AND (`groupid`='2') AND (`id`='1');
+UPDATE `locales_creature_text` SET `entry`='18831', `groupid`='2', `id`='2', `text_loc6`='¡No os temo!' WHERE (`entry`='18831') AND (`groupid`='2') AND (`id`='2');
+UPDATE `locales_creature_text` SET `entry`='18831', `groupid`='2', `id`='3', `text_loc6`='¡Bien, ahora lucháis contra mi!' WHERE (`entry`='18831') AND (`groupid`='2') AND (`id`='3');
+UPDATE `locales_creature_text` SET `entry`='18831', `groupid`='3', `id`='0', `text_loc6`='¡No has sido tan resistente al final!' WHERE (`entry`='18831') AND (`groupid`='3') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='18831', `groupid`='3', `id`='1', `text_loc6`='%s ríe.' WHERE (`entry`='18831') AND (`groupid`='3') AND (`id`='1');
+UPDATE `locales_creature_text` SET `entry`='18831', `groupid`='3', `id`='2', `text_loc6`='¡Maulgar es el rey!' WHERE (`entry`='18831') AND (`groupid`='3') AND (`id`='2');
+UPDATE `locales_creature_text` SET `entry`='18831', `groupid`='4', `id`='0', `text_loc6`='Gruul... os aplastará.' WHERE (`entry`='18831') AND (`groupid`='4') AND (`id`='0');
+
+-- Gruul el Asesino de Dragones --
+UPDATE `locales_creature_text` SET `entry`='19044', `groupid`='0', `id`='0', `text_loc6`='Venid... y morid.' WHERE (`entry`='19044') AND (`groupid`='0') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='19044', `groupid`='1', `id`='0', `text_loc6`='Huid.' WHERE (`entry`='19044') AND (`groupid`='1') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='19044', `groupid`='1', `id`='1', `text_loc6`='No hay escapatoria.' WHERE (`entry`='19044') AND (`groupid`='1') AND (`id`='1');
+UPDATE `locales_creature_text` SET `entry`='19044', `groupid`='2', `id`='0', `text_loc6`='Quedaos.' WHERE (`entry`='19044') AND (`groupid`='2') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='19044', `groupid`='2', `id`='1', `text_loc6`='Suplicad por vuestras vidas.' WHERE (`entry`='19044') AND (`groupid`='2') AND (`id`='1');
+UPDATE `locales_creature_text` SET `entry`='19044', `groupid`='3', `id`='0', `text_loc6`='No más.' WHERE (`entry`='19044') AND (`groupid`='3') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='19044', `groupid`='3', `id`='1', `text_loc6`='Indigno.' WHERE (`entry`='19044') AND (`groupid`='3') AND (`id`='1');
+UPDATE `locales_creature_text` SET `entry`='19044', `groupid`='3', `id`='2', `text_loc6`='Muere.' WHERE (`entry`='19044') AND (`groupid`='3') AND (`id`='2');
+UPDATE `locales_creature_text` SET `entry`='19044', `groupid`='4', `id`='0', `text_loc6`='Aaargh...' WHERE (`entry`='19044') AND (`groupid`='4') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='19044', `groupid`='5', `id`='0', `text_loc6`='¡%s crece!' WHERE (`entry`='19044') AND (`groupid`='5') AND (`id`='0');
