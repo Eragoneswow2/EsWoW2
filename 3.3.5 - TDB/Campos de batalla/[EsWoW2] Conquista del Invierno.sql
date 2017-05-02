aa0494dd865bb8d41c24df66fb103668bd1eaabc
Eragoneswow2 <david_7_8_7@hotmail.com>
2017-05-03 01:37:58 +0200

    Rework completo de Conquista del Invierno, con nuevos textos y entradas correctas.

---
+++ 3.3.5 - TDB/Campos de batalla/[EsWoW2] Conquista del Invierno.sql
@@ -0,0 +1,54 @@
+﻿
+-- CONQUISTA DEL INVIERNO --
+
+-- Textos generales del campo de batalla --
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='1', `id`='0', `text_loc6`='¡Han dañado la torre sur!' WHERE (`entry`='15214') AND (`groupid`='1') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='2', `id`='0', `text_loc6`='¡Han destruido la torre sur!' WHERE (`entry`='15214') AND (`groupid`='2') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='3', `id`='0', `text_loc6`='¡Han dañado la torre este!' WHERE (`entry`='15214') AND (`groupid`='3') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='4', `id`='0', `text_loc6`='¡Han destruido la torre este!' WHERE (`entry`='15214') AND (`groupid`='4') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='5', `id`='0', `text_loc6`='¡Han dañado la torre oeste!' WHERE (`entry`='15214') AND (`groupid`='5') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='6', `id`='0', `text_loc6`='¡Han destruido la torre oeste!' WHERE (`entry`='15214') AND (`groupid`='6') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='7', `id`='0', `text_loc6`='¡Han dañado la torre noroeste de la fortaleza!' WHERE (`entry`='15214') AND (`groupid`='7') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='8', `id`='0', `text_loc6`='¡Han destruido la torre noroeste de la fortaleza!' WHERE (`entry`='15214') AND (`groupid`='8') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='9', `id`='0', `text_loc6`='¡Han dañado la torre sudeste de la fortaleza!' WHERE (`entry`='15214') AND (`groupid`='9') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='10', `id`='0', `text_loc6`='¡Han destruido la torre sudeste de la fortaleza!' WHERE (`entry`='15214') AND (`groupid`='10') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='11', `id`='0', `text_loc6`='¡La Alianza ha atacado el Taller de Asedio del Templo Quebrado!' WHERE (`entry`='15214') AND (`groupid`='11') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='12', `id`='0', `text_loc6`='¡El Taller de Asedio del Templo Quebrado ha sido tomado por la Alianza!' WHERE (`entry`='15214') AND (`groupid`='12') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='13', `id`='0', `text_loc6`='¡La Horda ha atacado el Taller de Asedio del Templo Quebrado!' WHERE (`entry`='15214') AND (`groupid`='13') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='14', `id`='0', `text_loc6`='¡El Taller de Asedio del Templo Quebrado ha sido tomado por la Horda!' WHERE (`entry`='15214') AND (`groupid`='14') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='15', `id`='0', `text_loc6`='¡La Alianza ha atacado el Taller de Asedio Chispa Oriental!' WHERE (`entry`='15214') AND (`groupid`='15') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='16', `id`='0', `text_loc6`='¡El Taller de Asedio Chispa Oriental ha sido capturado por la Alianza!' WHERE (`entry`='15214') AND (`groupid`='16') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='17', `id`='0', `text_loc6`='¡La Horda ha atacado el Taller de Asedio Chispa Oriental!' WHERE (`entry`='15214') AND (`groupid`='17') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='18', `id`='0', `text_loc6`='¡El Taller de Asedio Chispa Oriental ha sido capturado por la Horda!' WHERE (`entry`='15214') AND (`groupid`='18') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='19', `id`='0', `text_loc6`='¡La Alianza ha atacado el Taller de Asedio del Anillo Sumergido!' WHERE (`entry`='15214') AND (`groupid`='19') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='20', `id`='0', `text_loc6`='¡El Taller de Asedio del Anillo Sumergido ha sido tomado por la Alianza!' WHERE (`entry`='15214') AND (`groupid`='20') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='21', `id`='0', `text_loc6`='¡La Horda ha atacado el Taller de Asedio del Anillo Sumergido!' WHERE (`entry`='15214') AND (`groupid`='21') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='22', `id`='0', `text_loc6`='¡El Taller de Asedio del Anillo Sumergido ha sido tomado por la Horda!' WHERE (`entry`='15214') AND (`groupid`='22') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='23', `id`='0', `text_loc6`='¡La Alianza ha atacado el Taller de Asedio Chispa Occidental!' WHERE (`entry`='15214') AND (`groupid`='23') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='24', `id`='0', `text_loc6`='¡El Taller de Asedio Chispa Occidental ha sido capturado por la Alianza!' WHERE (`entry`='15214') AND (`groupid`='24') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='25', `id`='0', `text_loc6`='¡La Horda ha atacado el Taller de Asedio Chispa Occidental!' WHERE (`entry`='15214') AND (`groupid`='25') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='26', `id`='0', `text_loc6`='¡El Taller de Asedio Chispa Occidental ha sido capturado por la Horda!' WHERE (`entry`='15214') AND (`groupid`='26') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='27', `id`='0', `text_loc6`='¡La batalla por el Lago Conquista del Invierno está a punto de comenzar!' WHERE (`entry`='15214') AND (`groupid`='27') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='28', `id`='0', `text_loc6`='¡Que comience la batalla!' WHERE (`entry`='15214') AND (`groupid`='28') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='29', `id`='0', `text_loc6`='¡La Alianza ha defendido la Fortaleza de Conquista del Invierno!' WHERE (`entry`='15214') AND (`groupid`='29') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='30', `id`='0', `text_loc6`='¡La Alianza ha tomado la Fortaleza de Conquista del Invierno!' WHERE (`entry`='15214') AND (`groupid`='30') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='31', `id`='0', `text_loc6`='¡La Horda ha defendido la Fortaleza de Conquista del Invierno!' WHERE (`entry`='15214') AND (`groupid`='31') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='32', `id`='0', `text_loc6`='¡La Horda ha tomado la Fortaleza de Conquista del Invierno!' WHERE (`entry`='15214') AND (`groupid`='32') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='33', `id`='0', `text_loc6`='¡Han dañado la torre noreste de la fortaleza!' WHERE (`entry`='15214') AND (`groupid`='33') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='34', `id`='0', `text_loc6`='¡Han destruido la torre noreste de la fortaleza!' WHERE (`entry`='15214') AND (`groupid`='34') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='35', `id`='0', `text_loc6`='¡Han dañado la torre sudoeste de la fortaleza!' WHERE (`entry`='15214') AND (`groupid`='35') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='36', `id`='0', `text_loc6`='¡Han destruido la torre sudoeste de la fortaleza!' WHERE (`entry`='15214') AND (`groupid`='36') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='37', `id`='0', `text_loc6`='Has alcanzado el Rango 1: Cabo' WHERE (`entry`='15214') AND (`groupid`='37') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15214', `groupid`='38', `id`='0', `text_loc6`='Has alcanzado el Rango 2: Teniente primero' WHERE (`entry`='15214') AND (`groupid`='38') AND (`id`='0');
+
+-- Comandante Dardosh --
+UPDATE `locales_creature_text` SET `entry`='31091', `groupid`='0', `id`='0', `text_loc6`='¡La primera de las torres de la Alianza ha caído! ¡Destruid las tres y aceleraremos su retirada!' WHERE (`entry`='31091') AND (`groupid`='0') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='31091', `groupid`='1', `id`='0', `text_loc6`='¡Lok\'tar! ¡La segunda torre ha caído! ¡Destruid la torre restante y aceleraremos su retirada!' WHERE (`entry`='31091') AND (`groupid`='1') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='31091', `groupid`='2', `id`='0', `text_loc6`='¡Las torres de la Alianza han caído! No les hemos dado tregua. ¡Acabad con lo que quede de su ejército!' WHERE (`entry`='31091') AND (`groupid`='2') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='31091', `groupid`='3', `id`='0', `text_loc6`='¡No muestres piedad, $N!' WHERE (`entry`='31091') AND (`groupid`='3') AND (`id`='0');
+
+-- Comandante Zanneth --
+UPDATE `locales_creature_text` SET `entry`='31036', `groupid`='0', `id`='0', `text_loc6`='¡La primera de las torres de la Horda ha caído! ¡Destruid las tres y aceleraremos su retirada!' WHERE (`entry`='31036') AND (`groupid`='0') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='31036', `groupid`='1', `id`='0', `text_loc6`='¡La segunda torre ha caído! ¡Destruid la torre restante y aceleraremos su retirada!' WHERE (`entry`='31036') AND (`groupid`='1') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='31036', `groupid`='2', `id`='0', `text_loc6`='¡Las torres de la Horda han caído! No les hemos dado tregua. ¡Acabad con lo que quede de su ejército!' WHERE (`entry`='31036') AND (`groupid`='2') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='31036', `groupid`='3', `id`='0', `text_loc6`='¡No muestres piedad, $N!' WHERE (`entry`='31036') AND (`groupid`='3') AND (`id`='0');
