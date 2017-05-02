aa473a558d3c4ded772f5fa25f9250e2262c8637
Eragoneswow2 <david_7_8_7@hotmail.com>
2017-05-02 14:19:42 +0200

    Añadida Ruinas de Ahn'Qiraj.

---
+++ 3.3.5 - TDB/Bandas CLASSIC/[EsWoW2] Ruinas de Ahn'Qiraj.sql
@@ -0,0 +1,39 @@
+﻿
+-- RUINAS DE AHN'QIRAJ --
+
+-- Osirio el Sinmarcas --
+UPDATE `locales_creature_text` SET `entry`='15339', `groupid`='0', `id`='0', `text_loc6`='¡Estoy rejuvenecido!' WHERE (`entry`='15339') AND (`groupid`='0') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15339', `groupid`='0', `id`='1', `text_loc6`='¡Mis poderes están renovados!' WHERE (`entry`='15339') AND (`groupid`='0') AND (`id`='1');
+UPDATE `locales_creature_text` SET `entry`='15339', `groupid`='0', `id`='2', `text_loc6`='¡Mis poderes han vuelto!' WHERE (`entry`='15339') AND (`groupid`='0') AND (`id`='2');
+UPDATE `locales_creature_text` SET `entry`='15339', `groupid`='1', `id`='0', `text_loc6`='¡Proteged la ciudad a todo coste!' WHERE (`entry`='15339') AND (`groupid`='1') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15339', `groupid`='1', `id`='1', `text_loc6`='¡A vuestros puestos! ¡Proteged la ciudad!' WHERE (`entry`='15339') AND (`groupid`='1') AND (`id`='1');
+UPDATE `locales_creature_text` SET `entry`='15339', `groupid`='1', `id`='2', `text_loc6`='Los intrusos serán eliminados.' WHERE (`entry`='15339') AND (`groupid`='1') AND (`id`='2');
+UPDATE `locales_creature_text` SET `entry`='15339', `groupid`='2', `id`='0', `text_loc6`='¡Que las arenas del desierto se eleven y apaguen la luz del sol!' WHERE (`entry`='15339') AND (`groupid`='2') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15339', `groupid`='3', `id`='0', `text_loc6`='Estáis acabados.' WHERE (`entry`='15339') AND (`groupid`='3') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15339', `groupid`='4', `id`='0', `text_loc6`='He... he... fracasado.' WHERE (`entry`='15339') AND (`groupid`='4') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15339', `groupid`='5', `id`='0', `text_loc6`='¡Han traspasado las puertas!' WHERE (`entry`='15339') AND (`groupid`='5') AND (`id`='0');
+
+-- General Rajaxx --
+UPDATE `locales_creature_text` SET `entry`='15341', `groupid`='0', `id`='0', `text_loc6`='¡Ha llegado el momento de nuestra venganza! ¡Que la oscuridad reine en los corazones de nuestros enemigos!' WHERE (`entry`='15341') AND (`groupid`='0') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15341', `groupid`='1', `id`='0', `text_loc6`='¡No vamos a esperar más encerrados detrás de estas puertas y muros de piedra! ¡Nadie nos negará nuestra venganza! ¡Los mismísimos dragones temblarán ante nuestra ira!' WHERE (`entry`='15341') AND (`groupid`='1') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15341', `groupid`='2', `id`='0', `text_loc6`='¡El miedo es para el enemigo! ¡El miedo y la muerte!' WHERE (`entry`='15341') AND (`groupid`='2') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15341', `groupid`='3', `id`='0', `text_loc6`='¡Corzocelada gimoteará y suplicará por su vida, tal y como hizo el cachorro de su hijo! ¡Mil años de injusticia terminarán este día!' WHERE (`entry`='15341') AND (`groupid`='3') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15341', `groupid`='4', `id`='0', `text_loc6`='¡Fandral! ¡Ha llegado tu hora! ¡Ve a esconderte en el Sueño Esmeralda y reza para que nunca te encontremos!' WHERE (`entry`='15341') AND (`groupid`='4') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15341', `groupid`='5', `id`='0', `text_loc6`='¡Idiota! ¡Yo mismo te mataré!' WHERE (`entry`='15341') AND (`groupid`='5') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15341', `groupid`='6', `id`='0', `text_loc6`='¡Atacad y haced que paguen caro por todo esto!' WHERE (`entry`='15341') AND (`groupid`='6') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15341', `groupid`='7', `id`='0', `text_loc6`='¡Aplastadlos! ¡Sacadlos de aquí!' WHERE (`entry`='15341') AND (`groupid`='7') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15341', `groupid`='8', `id`='0', `text_loc6`='¡No dudéis! ¡Destruidlos!' WHERE (`entry`='15341') AND (`groupid`='8') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15341', `groupid`='9', `id`='0', `text_loc6`='¡Guerreros, Capitanes, seguid luchando!' WHERE (`entry`='15341') AND (`groupid`='9') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15341', `groupid`='10', `id`='0', `text_loc6`='¡No vale la pena perder mi tiempo en ti, $N!' WHERE (`entry`='15341') AND (`groupid`='10') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15341', `groupid`='11', `id`='0', `text_loc6`='¡Tu último suspiro!' WHERE (`entry`='15341') AND (`groupid`='11') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15341', `groupid`='12', `id`='0', `text_loc6`='Pronto conoceréis el precio de vuestras intromisiones, mortales... El amo está casi entero... y cuando se levante, vuestro mundo cesará.' WHERE (`entry`='15341') AND (`groupid`='12') AND (`id`='0');
+
+-- Moam --
+UPDATE `locales_creature_text` SET `entry`='15340', `groupid`='0', `id`='0', `text_loc6`='%s siente tu miedo.' WHERE (`entry`='15340') AND (`groupid`='0') AND (`id`='0');
+UPDATE `locales_creature_text` SET `entry`='15340', `groupid`='1', `id`='0', `text_loc6`='¡%s se cubre con energía!' WHERE (`entry`='15340') AND (`groupid`='1') AND (`id`='0');
+
+-- Buru el Manducador --
+UPDATE `locales_creature_text` SET `entry`='15370', `groupid`='0', `id`='0', `text_loc6`='¡%s mira a $N!' WHERE (`entry`='15370') AND (`groupid`='0') AND (`id`='0');
+
+-- Ayamiss el Cazador --
+UPDATE `locales_creature_text` SET `entry`='15369', `groupid`='0', `id`='0', `text_loc6`='¡%s entra en frenesí!' WHERE (`entry`='15369') AND (`groupid`='0') AND (`id`='0');
