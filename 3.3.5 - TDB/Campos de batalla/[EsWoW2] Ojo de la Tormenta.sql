ec761cade2b0dec0ab3597c1341ebb3a0b20836b
Eragoneswow2 <david_7_8_7@hotmail.com>
2017-05-03 11:54:24 +0200

    Añadido campo de batalla Ojo de la Tormenta.

---
+++ 3.3.5 - TDB/Campos de batalla/[EsWoW2] Ojo de la Tormenta.sql
@@ -0,0 +1,32 @@
+﻿
+-- OJO DE LA TORMENTA --
+
+-- Textos generales del campo de batalla --
+UPDATE trinity_string SET `content_loc6`='¡La batalla comienza en 1 minuto!' WHERE entry=636;
+UPDATE trinity_string SET `content_loc6`='¡La batalla comienza en 30 segundos!' WHERE entry=637;
+UPDATE trinity_string SET `content_loc6`='¡La batalla ha comenzado!' WHERE entry=638;
+UPDATE trinity_string SET `content_loc6`='¡La Alianza controla la Torre de los Magos!' WHERE entry=667;
+UPDATE trinity_string SET `content_loc6`='¡La Horda controla la Torre de los Magos!' WHERE entry=668;
+UPDATE trinity_string SET `content_loc6`='¡La Alianza controla las Ruinas Draenei!' WHERE entry=669;
+UPDATE trinity_string SET `content_loc6`='¡La Horda controla las Ruinas Draenei!' WHERE entry=670;
+UPDATE trinity_string SET `content_loc6`='¡La Alianza controla la Torre de los Elfos de Sangre!' WHERE entry=671;
+UPDATE trinity_string SET `content_loc6`='¡La Horda controla la Torre de los Elfos de Sangre!' WHERE entry=672;
+UPDATE trinity_string SET `content_loc6`='¡La Alianza controla las Ruinas del Atracador Vil!' WHERE entry=673;
+UPDATE trinity_string SET `content_loc6`='¡La Horda controla las Ruinas del Atracador Vil!' WHERE entry=674;
+UPDATE trinity_string SET `content_loc6`='¡La Alianza ha perdido el control de la Torre de los Magos!' WHERE entry=675;
+UPDATE trinity_string SET `content_loc6`='¡La Horda ha perdido el control de la Torre de los Magos!' WHERE entry=676;
+UPDATE trinity_string SET `content_loc6`='¡La Alianza ha perdido el control de las Ruinas Draenei!' WHERE entry=677;
+UPDATE trinity_string SET `content_loc6`='¡La Horda ha perdido el control de las Ruinas Draenei!' WHERE entry=678;
+UPDATE trinity_string SET `content_loc6`='¡La Alianza ha perdido el control de la Torre de los Elfos de Sangre!' WHERE entry=679;
+UPDATE trinity_string SET `content_loc6`='¡La Horda ha perdido el control de la Torre de los Elfos de Sangre!' WHERE entry=680;
+UPDATE trinity_string SET `content_loc6`='¡La Alianza ha perdido el control de las Ruinas del Atracador Vil!' WHERE entry=681;
+UPDATE trinity_string SET `content_loc6`='¡La Horda ha perdido el control de las Ruinas del Atracador Vil!' WHERE entry=682;
+UPDATE trinity_string SET `content_loc6`='¡%s ha tomado la bandera!' WHERE entry=683;
+UPDATE trinity_string SET `content_loc6`='¡%s ha capturado la bandera!' WHERE entry=684; -- Necesito que alguien confirme que sale el nombre del jugador que captura la bandera --
+UPDATE trinity_string SET `content_loc6`='¡%s ha capturado la bandera!' WHERE entry=685; -- Necesito que alguien confirme que sale el nombre del jugador que captura la bandera --
+UPDATE trinity_string SET `content_loc6`='¡Ha caído la bandera!' WHERE entry=686;
+UPDATE trinity_string SET `content_loc6`='La bandera se ha restablecido.' WHERE entry=687;
+UPDATE trinity_string SET `content_loc6`='¡Victoria para la Alianza!' WHERE entry=600;
+UPDATE trinity_string SET `content_loc6`='¡Victoria para la Horda!' WHERE entry=601;
+UPDATE trinity_string SET `content_loc6`='¡La Alianza ha reunido más de $1776W recursos y está próxima a la victoria!' WHERE entry=664;
+UPDATE trinity_string SET `content_loc6`='¡La Horda ha reundio más de $1777W recursos y está próxima a la victoria!' WHERE entry=665;
