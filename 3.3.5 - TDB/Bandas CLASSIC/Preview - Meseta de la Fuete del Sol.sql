ae643893dc7e8c89a98b495b1ff7baa9ded6f9f6
Eragoneswow2 <david_7_8_7@hotmail.com>
2017-05-10 01:39:12 +0200

    Preview - Meseta de la Fuete del Sol
    Preview - Meseta de la Fuente del Sol, archivo por acabar de trabajar.

---
+++ 3.3.5 - TDB/Bandas CLASSIC/Preview - Meseta de la Fuete del Sol.sql
@@ -0,0 +1,101 @@
+
+**PREVIEW MLFS**
+
+**ARCHIVO NO FINAL**
+
+-- Protector Filosol --
+Presencia enemiga detectada.
+Detectada amenaza cercana. Abandonando modo de ahorro de energía.
+Unidad entrando en modo ahorro de energía.
+La unidad está ahora operativa y atacando objetivos.
+
+-- Explorador Filosol --
+¡Enemigos avistados! ¡Atacad mientras intento activar un protector!
+
+-- Kalecgos --
+¡Ya no seré un esclavo de Malygos! ¡Retadme y seréis destruidos!
+¡Vuestro dolor no ha hecho más que comenzar!
+Estaré en deuda con vosotros eternamente. Cuando hayamos triunfado sobre Kil'jaeden, este mundo también lo estará.
+¡Kil'jaeden debe ser destruido!
+Madrigosa merecía un destino mejor. Hicisteis lo que teníais que hacer, pero esta batalla aún no ha acabado.
+¡Anveena, debes despertar! ¡Este mundo te necesita!
+¡Déjate llevar! ¡Tienes que convertirte en lo que el destino ha decidido que debes ser! ¡Es el momento Anveena!
+¡Anveena, te quiero! ¡Concéntrate en mi voz! ¡Hazlo por mí! ¡Solo tú puedes purificar La Fuente del Sol!
+¡He canalizado todo lo que puedo! ¡El poder está en vuestras manos!
+Adiós, Anveena, amor mío. Pocos recordarán tu nombre, pero hoy has cambiado el destino. Lo que antes estaba corrupto, ahora está purificado. Héroes, no dejéis que su sacrificio sea en vano.
+
+-- Sathrovarr el Corruptor --
+¡Sathrovarr induce a Malygos a un estado de ira enloquecida!
+¡Miraré mientras sangráis!
+¡Me deleito en vuestra miseria!
+¡Yo... nunca... pierdo!
+
+-- Madrigosa --
+¡Esperad, amigos! ¡Tenemos información que obtener antes de que este demonio se enfrente a su destino!
+¿Dónde está Anveena, demonio? ¿Qué ha sido de Kalec?
+¡Me dirás dónde están!
+¡Habla! ¡Me estoy hartando de preguntar!
+Malygos, señor... hice... lo que... pude.
+
+-- Brutallus --
+¡Endebles lagartijas! ¡La muerte es la única respuesta que encontraréis aquí!
+¡Bah! ¡Vuestra magia es débil!
+¡Te aplastaré!
+¡Ha sido divertido, pero sigue esperando un auténtico reto!
+¡Venid a probar suerte!
+¡Ah, más corderos al matadero!
+¡Aggh! ¡Bien hecho! Esto se pone... interesante...
+¡Otro día, otra gloriosa batalla!
+
+-- Brumavil --
+¡Gloria a Kil'jaeden! ¡Muerte a sus oponentes!
+¡Que este sea vuestro último aliento!
+¡Soy más fuerte que nunca!
+%s respira hondo.
+¡Kil'jaeden... prevalecerá!
+
+-- Comandante Espada Sombría --
+¡Traed a los diablillos!
+
+-- Vencedor Espada Sombría --
+¡Intrusos! ¡No dejéis que entren en el Sagrario!
+
+-- Kil'jaeden --
+¡Vaciad a la chica! ¡Drenad su poder hasta que no quede más que una cáscara vacía!
+Los prescindibles han muerto. ¡Que así sea! ¡Ahora triunfaré donde Sargeras no lo logró! ¡Desangraré este despreciable mundo y me aseguraré mi puesto como verdadero maestro de la Legión Ardiente! ¡El final ha llegado! ¡Dejad que se desvele el misterio de este mundo!
+¡El enemigo está entre vosotros!
+¡No seré rechazado! ¡Este mundo caerá!
+¡No alberguéis falsas esperanzas! ¡No podéis ganar!
+
+-- Lady Sacrolash --
+Miseria... confusión... recelo. Estas son las señales.
+¡Alythess! ¡Tu fuego arde dentro de mí!
+Me... desvanezco.
+
+-- Bruja suprema Alythess --
+Depravación... odio... caos. Estos son los pilares.
+¡Fuego para ayudar a las sombras!
+¡Sacrolash!
+Der'ek... manul.
+
+-- Anveena --
+Ahora solo sirvo al maestro.
+Pero estoy... perdida. No encuentro el camino de vuelta.
+Kalec... ¿Kalec?
+¡La pesadilla ha terminado! ¡El hechizo se ha roto! ¡Adiós Kalec, amor mío!
+
+-- Profeta Velen --
+Héroes mortales, vuestra victoria de hoy aquí fue profetizada hace mucho. El angustiado grito de derrota de mi hermano resonará en el universo y llevará una esperanza renovada a todos los que aún se enfrentan a la Legión Ardiente.
+Mientras se acerca la derrota definitiva de la Legión, sentíos orgullosos sabiendo que habéis salvado incontables mundos de la llama.
+Del mimo modo que marca un final, este día también anuncia un nuevo comienzo...
+La criatura Entropius, a quién os habéis visto obligados a destruir, fue otrora el noble naaru M'uru. En vida, M'uru canalizó grandes energías de LUZ y ESPERANZA. Durante algún tiempo, algunos descaminados intentaron robar esas energías...
+¡La buena noticia es que he recuperado la chispa del noble naaru del lugar en que cayó! Donde hay fe nunca se pierde la esperanza, joven elfa de sangre.
+¡Observad ahora, mortales, el CORAZÓN DE M'URU! Inmaculado. Bañado por la luz de la creación, como en el amanecer.
+Con el tiempo, la luz y la esperanza que contiene renacerán para ser más que esta simple fuente de poder... Quizá revivan el alma de una nación.
+La salvación, joven. Es lo que nos espera a todos.
+Adiós...
+
+-- Lady Liadrin --
+Nuestra arrogancia fue imperdonable. Condenamos a uno de los seres más nobles jamás conocidos. Nunca expiaremos este pecado.
+¿Es posible...?
+¡Benditos antepasados! Lo siento... tanto amor... tanta gracia... no hay... palabras... es imposible describirlo...
