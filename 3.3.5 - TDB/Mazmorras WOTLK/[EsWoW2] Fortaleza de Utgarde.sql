00ad8bb93a12547d111b007231d3d38a41fdaaed
Eragoneswow2 <david_7_8_7@hotmail.com>
2017-05-13 14:45:02 +0200

    Empezando con las mazmorras, y empezando con los archivos base xxxx_locale de cada la tabla.

---
+++ 3.3.5 - TDB/Mazmorras WOTLK/[EsWoW2] Fortaleza de Utgarde.sql
@@ -0,0 +1,49 @@
+﻿
+-- FORTALEZA DE UTGARDE --
+
+/* Textos faltantes en la base de datos.
+
+-- Machacahuesos Desuelladragones --
+¡Será un placer destriparte!
+
+*/
+
+-- Trabajador de metal Desuelladragones --
+UPDATE `creature_text_locale` SET `CreatureID`='24078', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡%s entra en frenesí!' WHERE (`CreatureID`='24078') AND (`GroupID`='0') AND (`ID`='0');
+
+-- Forjador de armas Desuelladragones --
+UPDATE `creature_text_locale` SET `CreatureID`='24080', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡Me das asco!' WHERE (`CreatureID`='24080') AND (`GroupID`='0') AND (`ID`='0');
+
+-- Príncipe Keleseth --
+UPDATE `creature_text_locale` SET `CreatureID`='23953', `GroupID`='1', `ID`='0', `Locale`='esES', `Text`='¡Vuestra sangre es mía!' WHERE (`CreatureID`='23953') AND (`GroupID`='1') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='23953', `GroupID`='2', `ID`='0', `Locale`='esES', `Text`='¡Aranal, ledel! ¡Su muerte será la vuestra!' WHERE (`CreatureID`='23953') AND (`GroupID`='2') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='23953', `GroupID`='3', `ID`='0', `Locale`='esES', `Text`='No  tan rápido.' WHERE (`CreatureID`='23953') AND (`GroupID`='3') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='23953', `GroupID`='4', `ID`='0', `Locale`='esES', `Text`='%s lanza Tumba de Escarcha a $N.' WHERE (`CreatureID`='23953') AND (`GroupID`='4') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='23953', `GroupID`='5', `ID`='0', `Locale`='esES', `Text`='Me reúno... con la noche.' WHERE (`CreatureID`='23953') AND (`GroupID`='5') AND (`ID`='0');
+
+-- Skarvald el Constructor --
+UPDATE `creature_text_locale` SET `CreatureID`='24200', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡Dalronn! ¡A ver si puedes reunir el valor para unirte a mi ataque!' WHERE (`CreatureID`='24200') AND (`GroupID`='0') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='24200', `GroupID`='1', `ID`='0', `Locale`='esES', `Text`='No he... terminado... aún.' WHERE (`CreatureID`='24200') AND (`GroupID`='1') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='24200', `GroupID`='2', `ID`='0', `Locale`='esES', `Text`='Una muerte de un guerrero.' WHERE (`CreatureID`='24200') AND (`GroupID`='2') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='24200', `GroupID`='3', `ID`='0', `Locale`='esES', `Text`='¡Jarggn olkt!' WHERE (`CreatureID`='24200') AND (`GroupID`='3') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='24200', `GroupID`='4', `ID`='0', `Locale`='esES', `Text`='¡Buá! ¿Qué tipo de nigromante permite que lo detenga la muerte? ¡Sabía que no valías nada!' WHERE (`CreatureID`='24200') AND (`GroupID`='4') AND (`ID`='0');
+
+-- Dalronn el Controlador --
+UPDATE `creature_text_locale` SET `CreatureID`='27389', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡No te pongas a valorar la situación, imbécil! ¡Solo pelea!' WHERE (`CreatureID`='27389') AND (`GroupID`='0') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='27389', `GroupID`='1', `ID`='0', `Locale`='esES', `Text`='Hasta... la... vista...' WHERE (`CreatureID`='27389') AND (`GroupID`='1') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='27389', `GroupID`='2', `ID`='0', `Locale`='esES', `Text`='No hay... mayor... gloria.' WHERE (`CreatureID`='27389') AND (`GroupID`='2') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='27389', `GroupID`='3', `ID`='0', `Locale`='esES', `Text`='Aún podéis servirme.' WHERE (`CreatureID`='27389') AND (`GroupID`='3') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='27389', `GroupID`='4', `ID`='0', `Locale`='esES', `Text`='Skarvald, ¡babosa incompetente! ¡Vuelve y haz algo útil!' WHERE (`CreatureID`='27389') AND (`GroupID`='4') AND (`ID`='0');
+
+-- Ingvar el Desvalijador --
+UPDATE `creature_text_locale` SET `CreatureID`='23954', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡Me pintaré el rostro con vuestra sangre!' WHERE (`CreatureID`='23954') AND (`GroupID`='0') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='23954', `GroupID`='1', `ID`='0', `Locale`='esES', `Text`='¡Mjul orm agn gjor!' WHERE (`CreatureID`='23954') AND (`GroupID`='1') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='23954', `GroupID`='2', `ID`='0', `Locale`='esES', `Text`='Mi vida por el... ¡dios de la muerte!' WHERE (`CreatureID`='23954') AND (`GroupID`='2') AND (`ID`='0');
+
+-- Annhylde la Invocadora --
+UPDATE `creature_text_locale` SET `CreatureID`='24068', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡Ingvar! Tu patético fracaso servirá como advertencia para todos... ¡estás condenado! ¡Levántate y lleva a cabo la voluntad del maestro!' WHERE (`CreatureID`='24068') AND (`GroupID`='0') AND (`ID`='0');
+
+-- Ingvar el Desvalijador (Fase 2) --
+UPDATE `creature_text_locale` SET `CreatureID`='23980', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡He vuelto! ¡Otra oportunidad para despedazarte el cráneo!' WHERE (`CreatureID`='23980') AND (`GroupID`='0') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='23980', `GroupID`='1', `ID`='0', `Locale`='esES', `Text`='¡Soy un guerrero nato!' WHERE (`CreatureID`='23980') AND (`GroupID`='1') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='23980', `GroupID`='2', `ID`='0', `Locale`='esES', `Text`='¡No! Puedo hacerlo... ¡mejor! Puedo...' WHERE (`CreatureID`='23980') AND (`GroupID`='2') AND (`ID`='0');
