095687ecb966337ead0b8c09d341a08da62fcea0
Eragoneswow2 <david_7_8_7@hotmail.com>
2017-06-29 14:14:15 +0200

    Añadida Salas Sethekk.

---
+++ 3.3.5 - TDB/Mazmorras TBC/[EsWoW2] Salas Sethekk.sql
@@ -0,0 +1,32 @@
+﻿
+-- SALAS SETHEKK --
+
+-- Tejeoscuro Syth --
+UPDATE `creature_text_locale` SET `CreatureID`='18472', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡Tengo mascotas ah-ogh, propias!' WHERE (`CreatureID`='18472') AND (`GroupID`='0') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='18472', `GroupID`='1', `ID`='0', `Locale`='esES', `Text`='¡Mmm... ahora, me toca a mí!' WHERE (`CreatureID`='18472') AND (`GroupID`='1') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='18472', `GroupID`='1', `ID`='1', `Locale`='esES', `Text`='¡Bonitas mascotas... raa-a-ak!' WHERE (`CreatureID`='18472') AND (`GroupID`='1') AND (`ID`='1');
+UPDATE `creature_text_locale` SET `CreatureID`='18472', `GroupID`='1', `ID`='2', `Locale`='esES', `Text`='Las bonitas mascotas tienen armas. No tan ra-ak-bonitas.' WHERE (`CreatureID`='18472') AND (`GroupID`='1') AND (`ID`='2');
+UPDATE `creature_text_locale` SET `CreatureID`='18472', `GroupID`='2', `ID`='0', `Locale`='esES', `Text`='¡Sí, efímera es la vida, raa-a-aak!' WHERE (`CreatureID`='18472') AND (`GroupID`='2') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='18472', `GroupID`='2', `ID`='1', `Locale`='esES', `Text`='Mmm... sé libre. ¡Aak!' WHERE (`CreatureID`='18472') AND (`GroupID`='2') AND (`ID`='1');
+UPDATE `creature_text_locale` SET `CreatureID`='18472', `GroupID`='3', `ID`='0', `Locale`='esES', `Text`='Mmm... se acabó la vida, se acabó el dolor. ¡A-ak!' WHERE (`CreatureID`='18472') AND (`GroupID`='3') AND (`ID`='0');
+
+-- Anzu --
+UPDATE `creature_text_locale` SET `CreatureID`='23035', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡Despertad hijos míos y ayudad a vuestro amo!' WHERE (`CreatureID`='23035') AND (`GroupID`='0') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='23035', `GroupID`='1', `ID`='0', `Locale`='esES', `Text`='Tus hechizos... Ke-kaw... son débiles... fáciles de volver en tu contra...' WHERE (`CreatureID`='23035') AND (`GroupID`='1') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='23035', `GroupID`='1', `ID`='1', `Locale`='esES', `Text`='¡Sí...invoca tus preciosos hechicitos, ak-a-ak!' WHERE (`CreatureID`='23035') AND (`GroupID`='1') AND (`ID`='1');
+
+-- Rey Garra Ikiss --
+UPDATE `creature_text_locale` SET `CreatureID`='18473', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡Ra-ak! Abalorios, sí. ¡Abalorios preciosos! ¡Ak! Poder, gran poder. ¡Ra-kaw! ¡Poder de los abalorios! ¡Ak-co!' WHERE (`CreatureID`='18473') AND (`GroupID`='0') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='18473', `GroupID`='1', `ID`='0', `Locale`='esES', `Text`='Mmmm, ¿declaráis la guerra a Ikiss? ¡Aa-ak!' WHERE (`CreatureID`='18473') AND (`GroupID`='1') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='18473', `GroupID`='1', `ID`='1', `Locale`='esES', `Text`='Ikiss os corta, preciosos-ak-a-ak-, os rebana, ¡sí!' WHERE (`CreatureID`='18473') AND (`GroupID`='1') AND (`ID`='1');
+UPDATE `creature_text_locale` SET `CreatureID`='18473', `GroupID`='1', `ID`='2', `Locale`='esES', `Text`='¡No hay escapatoria para aogh-para vosotros!' WHERE (`CreatureID`='18473') AND (`GroupID`='1') AND (`ID`='2');
+UPDATE `creature_text_locale` SET `CreatureID`='18473', `GroupID`='2', `ID`='0', `Locale`='esES', `Text`='¡Muere-ka! ¡No toques los abalorios!' WHERE (`CreatureID`='18473') AND (`GroupID`='2') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='18473', `GroupID`='2', `ID`='1', `Locale`='esES', `Text`='Mmm...' WHERE (`CreatureID`='18473') AND (`GroupID`='2') AND (`ID`='1');
+UPDATE `creature_text_locale` SET `CreatureID`='18473', `GroupID`='3', `ID`='0', `Locale`='esES', `Text`='Ikiss no -rak, rak-morirá...' WHERE (`CreatureID`='18473') AND (`GroupID`='3') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='18473', `GroupID`='4', `ID`='0', `Locale`='esES', `Text`='%s comienza a canalizar energía arcana...' WHERE (`CreatureID`='18473') AND (`GroupID`='4') AND (`ID`='0');
+
+-- Lakka --
+UPDATE `creature_text_locale` SET `CreatureID`='18956', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡Gracias por liberarme, $n! ¡Me voy a Shattrath!' WHERE (`CreatureID`='18956') AND (`GroupID`='0') AND (`ID`='0');
+
+-- Guardia cuervo Sethekk --
+UPDATE `creature_text_locale` SET `CreatureID`='18322', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡%s entra en frenesí!' WHERE (`CreatureID`='18322') AND (`GroupID`='0') AND (`ID`='0');
