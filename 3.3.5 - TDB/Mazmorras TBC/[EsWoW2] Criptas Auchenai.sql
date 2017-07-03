1107391ef51825e7cc227ab9e6a6aac9c50d7774
Eragoneswow2 <david_7_8_7@hotmail.com>
2017-07-03 16:03:23 +0200

    Añadida Cripta de Auchenai.

---
+++ 3.3.5 - TDB/Mazmorras TBC/[EsWoW2] Criptas Auchenai.sql
@@ -0,0 +1,20 @@
+﻿
+-- CRIPTAS AUCHENAI --
+
+-- Shirrak el Vigía de los Muertos --
+UPDATE `creature_text_locale` SET `CreatureID`='18371', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡%s se centra en $N!' WHERE (`CreatureID`='18371') AND (`GroupID`='0') AND (`ID`='0');
+
+-- Exarca Maladaar --
+UPDATE `creature_text_locale` SET `CreatureID`='18373', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='Habéis profanado el lugar de descanso de nuestros antepasados y por esta ofensa, no os queda más que castigo. Me alegra que hayáis venido a un lugar lleno de muertos... porque pronto os uniréis a ellos.' WHERE (`CreatureID`='18373') AND (`GroupID`='0') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='18373', `GroupID`='1', `ID`='0', `Locale`='esES', `Text`='¡Levantaos, hermanos caídos! ¡Tomad forma y luchad!' WHERE (`CreatureID`='18373') AND (`GroupID`='1') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='18373', `GroupID`='2', `ID`='0', `Locale`='esES', `Text`='¡Lo pagaréis con vuestra vida!' WHERE (`CreatureID`='18373') AND (`GroupID`='2') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='18373', `GroupID`='2', `ID`='1', `Locale`='esES', `Text`='¡Ya no hay vuelta atrás!' WHERE (`CreatureID`='18373') AND (`GroupID`='2') AND (`ID`='1');
+UPDATE `creature_text_locale` SET `CreatureID`='18373', `GroupID`='2', `ID`='2', `Locale`='esES', `Text`='¡Cumplid vuestra penitencia!' WHERE (`CreatureID`='18373') AND (`GroupID`='2') AND (`ID`='2');
+UPDATE `creature_text_locale` SET `CreatureID`='18373', `GroupID`='3', `ID`='0', `Locale`='esES', `Text`='Que se nuble vuestra mente.' WHERE (`CreatureID`='18373') AND (`GroupID`='3') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='18373', `GroupID`='3', `ID`='1', `Locale`='esES', `Text`='Mirad fijamente la oscuridad de vuestra alma.' WHERE (`CreatureID`='18373') AND (`GroupID`='3') AND (`ID`='1');
+UPDATE `creature_text_locale` SET `CreatureID`='18373', `GroupID`='4', `ID`='0', `Locale`='esES', `Text`='¡Estas paredes serán tu tumba!' WHERE (`CreatureID`='18373') AND (`GroupID`='4') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='18373', `GroupID`='4', `ID`='1', `Locale`='esES', `Text`='Ahora te quedarás... toda la eternidad.' WHERE (`CreatureID`='18373') AND (`GroupID`='4') AND (`ID`='1');
+UPDATE `creature_text_locale` SET `CreatureID`='18373', `GroupID`='5', `ID`='0', `Locale`='esES', `Text`='Este es... mi sitio.' WHERE (`CreatureID`='18373') AND (`GroupID`='5') AND (`ID`='0');
+
+-- D'ore --
+UPDATE `creature_text_locale` SET `CreatureID`='19412', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='Una brisa fresca acaricia tu cara. Un murmullo agradable y melódico resuena en la sala.' WHERE (`CreatureID`='19412') AND (`GroupID`='0') AND (`ID`='0');
