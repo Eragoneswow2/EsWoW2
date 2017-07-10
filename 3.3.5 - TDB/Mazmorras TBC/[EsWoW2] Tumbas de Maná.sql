8421b298e0eb52221035f4cff8afe89ada5287c4
Eragoneswow2 <david_7_8_7@hotmail.com>
2017-07-10 11:44:37 +0200

    Añadida Tumbas de Maná.

---
+++ 3.3.5 - TDB/Mazmorras TBC/[EsWoW2] Tumbas de Maná.sql
@@ -0,0 +1,24 @@
+﻿
+-- TUMBAS DE MANÁ --
+
+-- Pandemonius --
+UPDATE `creature_text_locale` SET `CreatureID`='18341', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='Me nutriré de... vuestra alma.' WHERE (`CreatureID`='18341') AND (`GroupID`='0') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='18341', `GroupID`='0', `ID`='1', `Locale`='esES', `Text`='Tanta... vida.' WHERE (`CreatureID`='18341') AND (`GroupID`='0') AND (`ID`='1');
+UPDATE `creature_text_locale` SET `CreatureID`='18341', `GroupID`='0', `ID`='2', `Locale`='esES', `Text`='No... os resistáis.' WHERE (`CreatureID`='18341') AND (`GroupID`='0') AND (`ID`='2');
+UPDATE `creature_text_locale` SET `CreatureID`='18341', `GroupID`='1', `ID`='0', `Locale`='esES', `Text`='Sí... tengo... el poder.' WHERE (`CreatureID`='18341') AND (`GroupID`='1') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='18341', `GroupID`='1', `ID`='1', `Locale`='esES', `Text`='Más... quiero más.' WHERE (`CreatureID`='18341') AND (`GroupID`='1') AND (`ID`='1');
+UPDATE `creature_text_locale` SET `CreatureID`='18341', `GroupID`='2', `ID`='0', `Locale`='esES', `Text`='Al Vacío... otra vez.' WHERE (`CreatureID`='18341') AND (`GroupID`='2') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='18341', `GroupID`='3', `ID`='0', `Locale`='esES', `Text`='%s se desliza en el vacío.' WHERE (`CreatureID`='18341') AND (`GroupID`='3') AND (`ID`='0');
+
+-- Príncipe-nexo Shaffar --
+UPDATE `creature_text_locale` SET `CreatureID`='18344', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¿Qué significa esto? Me tenéis que disculpar pero no esperaba compañía. Como podéis ver, estamos algo preocupados ahora mismo. Pero no importa. Puesto que soy un gentil anfitrión os atenderé... personalmente.' WHERE (`CreatureID`='18344') AND (`GroupID`='0') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='18344', `GroupID`='1', `ID`='0', `Locale`='esES', `Text`='No nos han presentado debidamente.' WHERE (`CreatureID`='18344') AND (`GroupID`='1') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='18344', `GroupID`='1', `ID`='1', `Locale`='esES', `Text`='Una batalla épica, ¡qué emocionante!' WHERE (`CreatureID`='18344') AND (`GroupID`='1') AND (`ID`='1');
+UPDATE `creature_text_locale` SET `CreatureID`='18344', `GroupID`='1', `ID`='2', `Locale`='esES', `Text`='¡He anhelado una aventura de verdad!' WHERE (`CreatureID`='18344') AND (`GroupID`='1') AND (`ID`='2');
+UPDATE `creature_text_locale` SET `CreatureID`='18344', `GroupID`='2', `ID`='0', `Locale`='esES', `Text`='Ha sido... divertido.' WHERE (`CreatureID`='18344') AND (`GroupID`='2') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='18344', `GroupID`='2', `ID`='1', `Locale`='esES', `Text`='Ahora nos decimos adiós.' WHERE (`CreatureID`='18344') AND (`GroupID`='2') AND (`ID`='1');
+UPDATE `creature_text_locale` SET `CreatureID`='18344', `GroupID`='3', `ID`='0', `Locale`='esES', `Text`='Os voy a enseñar cosas fascinantes.' WHERE (`CreatureID`='18344') AND (`GroupID`='3') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='18344', `GroupID`='4', `ID`='0', `Locale`='esES', `Text`='Debo... despedirme.' WHERE (`CreatureID`='18344') AND (`GroupID`='4') AND (`ID`='0');
+
+-- Asaltante de tumbas etéreo --
+UPDATE `creature_text_locale` SET `CreatureID`='18311', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡%s se enfurece!' WHERE (`CreatureID`='18311') AND (`GroupID`='0') AND (`ID`='0');
