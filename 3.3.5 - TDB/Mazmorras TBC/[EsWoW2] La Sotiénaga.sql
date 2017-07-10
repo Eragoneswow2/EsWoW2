106ace0e33247e1b2d06fa21d935c99ad9478493
Eragoneswow2 <david_7_8_7@hotmail.com>
2017-07-10 12:01:25 +0200

    Añadida La Sotiénaga.

---
+++ 3.3.5 - TDB/Mazmorras TBC/[EsWoW2] La Sotiénaga.sql
@@ -0,0 +1,42 @@
+﻿
+-- LA SOTIÉNAGA --
+
+-- Gha'zan --
+UPDATE `creature_text_locale` SET `CreatureID`='18105', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡%s se enfurece!' WHERE (`CreatureID`='18105') AND (`GroupID`='0') AND (`ID`='0');
+
+-- Musel'ek --
+UPDATE `creature_text_locale` SET `CreatureID`='17826', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡Bestia! ¡Obedéceme! ¡Mátalos de una vez!' WHERE (`CreatureID`='17826') AND (`GroupID`='0') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='17826', `GroupID`='1', `ID`='0', `Locale`='esES', `Text`='¡Lucharemos hasta la muerte!' WHERE (`CreatureID`='17826') AND (`GroupID`='1') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='17826', `GroupID`='1', `ID`='1', `Locale`='esES', `Text`='Acabaré con esto rápido...' WHERE (`CreatureID`='17826') AND (`GroupID`='1') AND (`ID`='1');
+UPDATE `creature_text_locale` SET `CreatureID`='17826', `GroupID`='1', `ID`='2', `Locale`='esES', `Text`='¡Acalah pek ecta!' WHERE (`CreatureID`='17826') AND (`GroupID`='1') AND (`ID`='2');
+UPDATE `creature_text_locale` SET `CreatureID`='17826', `GroupID`='2', `ID`='0', `Locale`='esES', `Text`='¡Krypta!' WHERE (`CreatureID`='17826') AND (`GroupID`='2') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='17826', `GroupID`='2', `ID`='1', `Locale`='esES', `Text`='Se acabó.' WHERE (`CreatureID`='17826') AND (`GroupID`='2') AND (`ID`='1');
+UPDATE `creature_text_locale` SET `CreatureID`='17826', `GroupID`='3', `ID`='0', `Locale`='esES', `Text`='Bien... hecho.' WHERE (`CreatureID`='17826') AND (`GroupID`='3') AND (`ID`='0');
+
+-- Aguijonero Lykul --
+UPDATE `creature_text_locale` SET `CreatureID`='19632', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡%s se enfurece!' WHERE (`CreatureID`='19632') AND (`GroupID`='0') AND (`ID`='0');
+
+-- Avispa Lykul --
+UPDATE `creature_text_locale` SET `CreatureID`='17732', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡%s entra en frenesí!' WHERE (`CreatureID`='17732') AND (`GroupID`='0') AND (`ID`='0');
+
+-- Gigante de la marisma --
+UPDATE `creature_text_locale` SET `CreatureID`='17723', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡%s se enfurece!' WHERE (`CreatureID`='17723') AND (`GroupID`='0') AND (`ID`='0');
+
+-- Guerrero de aleta cólera --
+UPDATE `creature_text_locale` SET `CreatureID`='17735', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡%s se enfurece!' WHERE (`CreatureID`='17735') AND (`GroupID`='0') AND (`ID`='0');
+
+-- Lancero Sangreoscura --
+UPDATE `creature_text_locale` SET `CreatureID`='17729', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡%s entra en frenesí!' WHERE (`CreatureID`='17729') AND (`GroupID`='0') AND (`ID`='0');
+
+-- Miembro de la tribu Sangreoscura --
+UPDATE `creature_text_locale` SET `CreatureID`='17728', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡%s entra en frenesí!' WHERE (`CreatureID`='17728') AND (`GroupID`='0') AND (`ID`='0');
+
+-- Rondador de la Sotiénaga --
+UPDATE `creature_text_locale` SET `CreatureID`='17725', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡%s crece al ver a $n!' WHERE (`CreatureID`='17725') AND (`GroupID`='0') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='17725', `GroupID`='1', `ID`='0', `Locale`='esES', `Text`='La fuerza de %s se desvanece.' WHERE (`CreatureID`='17725') AND (`GroupID`='1') AND (`ID`='0');
+
+-- Sanador Sangreoscura --
+UPDATE `creature_text_locale` SET `CreatureID`='17730', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡%s comienza una curación masiva!' WHERE (`CreatureID`='17730') AND (`GroupID`='0') AND (`ID`='0');
+
+-- Señor de la Sotiénaga --
+UPDATE `creature_text_locale` SET `CreatureID`='17734', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡%s entra en frenesí!' WHERE (`CreatureID`='17734') AND (`GroupID`='0') AND (`ID`='0');
