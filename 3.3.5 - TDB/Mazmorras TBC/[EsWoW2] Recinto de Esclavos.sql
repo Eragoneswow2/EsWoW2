6b530c1005ebe2b86567bf97235bd23e4409068b
Eragoneswow2 <david_7_8_7@hotmail.com>
2017-07-19 12:23:37 +0200

    Añadido Recinto de Esclavos.

---
+++ 3.3.5 - TDB/Mazmorras TBC/[EsWoW2] Recinto de Esclavos.sql
@@ -0,0 +1,41 @@
+﻿
+-- RECINTO DE ESCLAVOS --
+
+-- Mennu el Traidor --
+UPDATE `creature_text_locale` SET `CreatureID`='17941', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡El trabajo debe continuar!' WHERE (`CreatureID`='17941') AND (`GroupID`='0') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='17941', `GroupID`='0', `ID`='1', `Locale`='esES', `Text`='¡Lo habéis provocado vosotros!' WHERE (`CreatureID`='17941') AND (`GroupID`='0') AND (`ID`='1');
+UPDATE `creature_text_locale` SET `CreatureID`='17941', `GroupID`='0', `ID`='2', `Locale`='esES', `Text`='¡No me obliguéis a mataros!' WHERE (`CreatureID`='17941') AND (`GroupID`='0') AND (`ID`='2');
+UPDATE `creature_text_locale` SET `CreatureID`='17941', `GroupID`='1', `ID`='0', `Locale`='esES', `Text`='Tenía que hacerse...' WHERE (`CreatureID`='17941') AND (`GroupID`='1') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='17941', `GroupID`='1', `ID`='1', `Locale`='esES', `Text`='No deberías haber venido...' WHERE (`CreatureID`='17941') AND (`GroupID`='1') AND (`ID`='1');
+UPDATE `creature_text_locale` SET `CreatureID`='17941', `GroupID`='2', `ID`='0', `Locale`='esES', `Text`='Me... lo merezco...' WHERE (`CreatureID`='17941') AND (`GroupID`='2') AND (`ID`='0');
+
+-- Clamallamas del Anillo de la Tierra --
+UPDATE `creature_text_locale` SET `CreatureID`='25754', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='Ahune se retira. Sus defensas disminuyen.' WHERE (`CreatureID`='25754') AND (`GroupID`='0') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='25754', `GroupID`='1', `ID`='0', `Locale`='esES', `Text`='Ahune pronto volverá a la superficie.' WHERE (`CreatureID`='25754') AND (`GroupID`='1') AND (`ID`='0');
+
+-- Campeón Colmillo Torcido --
+UPDATE `creature_text_locale` SET `CreatureID`='17957', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡Intrusosss con el prisionero! ¡Mátalos!' WHERE (`CreatureID`='17957') AND (`GroupID`='0') AND (`ID`='0');
+
+-- Colaborador Colmillo Torcido --
+UPDATE `creature_text_locale` SET `CreatureID`='17962', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡%s entra en frenesí!' WHERE (`CreatureID`='17962') AND (`GroupID`='0') AND (`ID`='0');
+
+-- Tratante de esclavos Colmillo Torcido --
+UPDATE `creature_text_locale` SET `CreatureID`='17959', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡Demasiado pronto! ¡Aflojas demasiado pronto!' WHERE (`CreatureID`='17959') AND (`GroupID`='0') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='17959', `GroupID`='0', `ID`='1', `Locale`='esES', `Text`='¡Despierta! ¡Ahora levanta y vuelve al trabajo!' WHERE (`CreatureID`='17959') AND (`GroupID`='0') AND (`ID`='1');
+UPDATE `creature_text_locale` SET `CreatureID`='17959', `GroupID`='0', `ID`='2', `Locale`='esES', `Text`='¡Date prisa de una vez! ¡Cuanto más tardes, más te vas a arrepentir!' WHERE (`CreatureID`='17959') AND (`GroupID`='0') AND (`ID`='2');
+UPDATE `creature_text_locale` SET `CreatureID`='17959', `GroupID`='1', `ID`='0', `Locale`='esES', `Text`='¡¿Qué es esto?! ¡¿No te enseñaron nada papá y mamá?!' WHERE (`CreatureID`='17959') AND (`GroupID`='1') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='17959', `GroupID`='1', `ID`='1', `Locale`='esES', `Text`='¡Tú, vuelve al trabajo!' WHERE (`CreatureID`='17959') AND (`GroupID`='1') AND (`ID`='1');
+UPDATE `creature_text_locale` SET `CreatureID`='17959', `GroupID`='1', `ID`='2', `Locale`='esES', `Text`='Esto es terrible... ¡Se me cansan los brazos de tanto daros palizas, patanes perezosos!' WHERE (`CreatureID`='17959') AND (`GroupID`='1') AND (`ID`='2');
+UPDATE `creature_text_locale` SET `CreatureID`='17959', `GroupID`='2', `ID`='0', `Locale`='esES', `Text`='¡Ayudadme, esclavos!' WHERE (`CreatureID`='17959') AND (`GroupID`='2') AND (`ID`='0');
+
+-- Luma Cielomater --
+UPDATE `creature_text_locale` SET `CreatureID`='25697', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡La piedra de hielo se ha derretido!' WHERE (`CreatureID`='25697') AND (`GroupID`='0') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='25697', `GroupID`='1', `ID`='0', `Locale`='esES', `Text`='¡Ahune, tu fuerza ya no aumenta!' WHERE (`CreatureID`='25697') AND (`GroupID`='1') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='25697', `GroupID`='2', `ID`='0', `Locale`='esES', `Text`='¡Tu reino helado no tendrá lugar!' WHERE (`CreatureID`='25697') AND (`GroupID`='2') AND (`ID`='0');
+
+-- Naturalista Mordisco --
+UPDATE `creature_text_locale` SET `CreatureID`='17893', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡Eh! ¡Por aquí! Sí, por aquí... ¡¡¡Estoy en esta jaula!!!' WHERE (`CreatureID`='17893') AND (`GroupID`='0') AND (`ID`='0');
+UPDATE `creature_text_locale` SET `CreatureID`='17893', `GroupID`='1', `ID`='0', `Locale`='esES', `Text`='¡Vaya! ¡Parece que todo el ruido que has hecho ha atraído algo de atención no deseada!' WHERE (`CreatureID`='17893') AND (`GroupID`='1') AND (`ID`='0');
+
+-- Skar'this el Invocador --
+UPDATE `creature_text_locale` SET `CreatureID`='40446', `GroupID`='0', `ID`='0', `Locale`='esES', `Text`='¡Cómo te atreves! ¡No podrás evitar la llegada de Lord Ahune!' WHERE (`CreatureID`='40446') AND (`GroupID`='0') AND (`ID`='0');
